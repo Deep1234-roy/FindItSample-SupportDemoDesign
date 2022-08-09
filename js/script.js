1202107444f1458a2224c6d2eaf377342bb54bc0
503322275 <deep.roy@ge.com>
2022-08-09 18:16:17 +0530

    Modifying the Reset Description Functionality and changing the button color after enabling

--- js/script.js
+++ js/script.js
@@ -4,6 +4,13 @@ var app = angular
             .module("MyApp",[])
             .controller("MyCTRL",function($scope){
 
+
+                $scope.regForm = true;
+                $scope.openRegistrationForm = function(){
+                    $scope.regForm = false;
+                    $scope.flag=false;
+                }
+
                 //when the user clicks on the button toggle between hiding and showing the dropdown content.
                 $scope.click = function(){
                     document.getElementById("myDropDown").classList.toggle("show");
@@ -13,9 +20,13 @@ var app = angular
                 var modal = document.getElementById("myModal");
                 var btn = document.getElementById("save-btn");
                 // Get the <span> element that closes the modal
-                var span = document.getElementsByClassName("close")[0];
-                // When the user clicks the button, open the modal 
-                btn.onclick = function() {
+                // var span = document.getElementsByClassName("close")[0];
+                // // When the user clicks the button, open the modal 
+                // btn.onclick = function() {
+                //     modal.style.display = "block";
+                // }
+
+                $scope.openModal = function(){
                     modal.style.display = "block";
                 }
 
@@ -25,6 +36,11 @@ var app = angular
                     $scope.formData.category = '--Select an issue type--';
                     $scope.formData.longDescription='';
                     $scope.formData.description='';
+                }
+
+                $scope.AddUser = function(){
+                    window.alert("You have successfully registered with us!!");
+                    $scope.regForm = true;
                 }
   
 
@@ -41,6 +57,7 @@ var app = angular
                 $scope.flag=false;
                 $scope.openForm=function(){
                     $scope.flag=true;
+                    $scope.regForm=true;
                 }
                 $scope.closeForm=function(){
                     $scope.flag=false;
@@ -59,8 +76,12 @@ var app = angular
                     longDescription: ''
                 }
                 $scope.resetDescription = function(){
-                    $scope.formData.longDescription='';
-                    $scope.formData.description='';
+                    if($scope.formData.category !== 'Other - Please Explain'){
+                        $scope.formData.longDescription = '';
+                    }else{
+                        $scope.formData.description = '';
+                        $scope.formData.longDescription ='';
+                    }
                 }
                 $scope.categories = [
                     { label: '--Select an issue type--', value: 'default' },
@@ -81,5 +102,19 @@ var app = angular
                     }
                     return false;
                 }
+
+                var saveBtn = document.getElementById("save-btn");
+                function colorChangeButton(){
+                    if(saveBtn.enabled == true){
+                        saveBtn.style.background='blue';
+                        
+                    }else{
+                        saveBtn.style.background='grey';
+                    }
+                }
                
             })
+
+app.service("RegistrationService",function(){
+    
+})
